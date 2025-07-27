f2edd9cf30ad223ce219619069720cbbd993c8e8
ritiksaxena124 <ritiksaxena124@gmail.com>
2025-07-27 13:10:40 +0530

    refactor(dev-scripts): simplify dev command in package.json for http and ws backends; add logging for WebSocket server events

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,12 @@
 import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 8080 });
 
+wss.on('listening', () => {
+    console.log("WebSocket server is listening on ws://localhost:8080");
+})
+
 wss.on('connection', ws => {
+    console.log("New client connected");
     ws.on('error', console.error);
     ws.on('message', message => {
         console.log(`Received message: ${message}`);
