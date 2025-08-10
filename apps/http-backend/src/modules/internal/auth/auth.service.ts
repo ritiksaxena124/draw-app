1e1753db76b51d0a97da8092d8ea6deb4e74e2fb
ritiksaxena124 <ritiksaxena124@gmail.com>
2025-08-10 22:57:08 +0530

    feat(auth): implement JWT authentication in login and WebSocket connection handling

--- apps/http-backend/src/modules/internal/auth/auth.service.ts
+++ apps/http-backend/src/modules/internal/auth/auth.service.ts
@@ -1,7 +1,41 @@
+import { HttpStatusCode, HttpStatusMsg } from "../../../constants/http-status.constant";
+import { ApiError } from "../../../lib/utils/api-error.util";
+import jwt from "jsonwebtoken";
 
 class AuthService {
-    async login(): Promise<null> {
-        return null;
+    async login() {
+        try {
+            const userId = 123;
+            const accessToken = jwt.sign({
+                userId
+            }, 'jwt secret', {
+                expiresIn: '1h'
+            }, (err, token) => {
+                if (err) {
+                    throw new ApiError(HttpStatusCode.INTERNAL_SERVER_ERROR, HttpStatusMsg.INTERNAL_SERVER_ERROR, err);
+                }
+                return token;
+            })
+
+            const refreshToken = jwt.sign({
+                userId
+            }, 'jwt secret', {
+                expiresIn: '7d'
+            }, (err, token) => {
+                if (err) {
+                    throw new ApiError(HttpStatusCode.INTERNAL_SERVER_ERROR, HttpStatusMsg.INTERNAL_SERVER_ERROR, err);
+                }
+                return token;
+            })
+
+            return {
+                accessToken,
+                refreshToken,
+            }
+
+        } catch (error) {
+            throw new ApiError(HttpStatusCode.INTERNAL_SERVER_ERROR, HttpStatusMsg.INTERNAL_SERVER_ERROR, error)
+        }
     }
 }
 
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,38 @@
 import { WebSocketServer } from 'ws';
+import jwt from 'jsonwebtoken';
+import { JsonWebTokenError } from 'jsonwebtoken';
+
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('listening', () => {
     console.log("WebSocket server is listening on ws://localhost:8080");
 })
 
-wss.on('connection', ws => {
+wss.on('connection', (ws, req) => {
+    // check if the request has a valid JWT token
+    const url = req.url;
+    if(!url) {
+        return; // No URL provided, cannot extract token
+    }
+    const token = new URLSearchParams(url?.split('?')[1] || '').get('token') || '';
+    if (!token) {
+        ws.close();
+        return;
+    }
+    // check if the token is valid
+    try {
+        jwt.verify(token, 'jwt secret');
+    } catch (err) {
+        if (err instanceof JsonWebTokenError) {
+            console.error("Invalid JWT token:", err.message);
+            ws.close();
+            return;
+        }
+        console.error("JWT verification error:", err);
+        ws.close();
+        return;
+    }
+
     console.log("New client connected");
     ws.on('error', console.error);
     ws.on('message', message => {
